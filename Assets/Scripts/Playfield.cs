7ede64a1487876eff4ac120517fda3a9abc332fa
BigWan <BigWan@users.noreply.github.com>
2018-07-09 09:50:10 +0800

    123

---
+++ Assets/Scripts/Playfield.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 数组表示俄罗斯方块的每个格子信息
+//  以左下角为[0,0]点，往右x++，往上y++
+/// </summary>
+public class Playfield : MonoBehaviour {
+
+    public int column = 10;
+    public int maxRow = 22;
+    public Transform cellPrefab;
+
+    public int[, ] fields;
+
+    public void SetField () {
+        fields = new int[maxRow, column];
+    }
+
+    // public int[,] fields;
+    public void InitField () {
+        for (int y = 0; y < maxRow; y++) {
+            for (int x = 0; x < column; x++) {
+                fields[x, y] = 0;
+            }
+        }
+    }
+
+    public void RenderGrid () {
+        for (int i = 0; i < maxRow; i++) {
+            for (int j = 0; j < column; j++) {
+                RenderCell (i, j);
+            }
+        }
+    }
+    public void RenderCell (int i, int j) {
+
+    }
+    public void CheckRows () {
+        List<int> r = new List<int> ();
+        for (int i = 0; i < maxRow; i++) {
+            int s = 0;
+            for (int j = 0; j < column; j++) {
+                s += fields[i, j];
+            }
+            if (s >= column) { // 这行满了
+                r.Add (i);
+            }
+        }
+    }
+
+    void CheckRow () {
+        for (int i = 0; i < 22; i++) {
+            CheckARow (i);
+        }
+    }
+
+    void CheckARow (int rowIndex) {
+
+    }
+}
---
+++ Assets/Scripts/TetrisShape.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 形状
+/// </summary>
+public class TetrisShape : MonoBehaviour {
+
+	public Transform[] cubes; 		// 块
+	public Vector3Int[] positions;	// 位置
+	public Transform pivot; 		// 旋转中心
+	public Playfield field;			// 格子
+
+	/// <summary>
+	/// 顺时针旋转每个块
+	/// </summary>
+	public void Rotation () {
+		// rotate cubes
+		foreach (var item in cubes) {
+			item.RotateAround (pivot.localPosition, Vector3.back, 90f);
+		}
+		// get v3 pos
+		for (int i = 0; i < positions.Length; i++) {
+			positions[i] = Vector3Int.RoundToInt( cubes[i].localPosition);
+
+		}
+	}
+
+	void MoveRight () {
+		Move (Vector3.right);
+	}
+
+	void MoveLeft () {
+		Move (Vector3.left);
+	}
+
+	/// <summary>
+	/// 朝一个方向移动
+	/// </summary>
+	/// <param name="dir">移动方向</param>
+	void Move (Vector3 dir) {
+		foreach (var item in cubes) {
+			item.localPosition = item.localPosition + dir;
+		}
+		pivot.localPosition = pivot.localPosition + dir;
+	}
+
+	void MoveDown () {
+		Move (Vector3.down);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Rotation ();
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			MoveDown ();
+		}
+		if(Input.GetKeyDown(KeyCode.C)){
+			Stop();
+		}
+	}
+
+	public bool TryMove () {
+		return false;
+	}
+
+	// 停止
+	public void Stop(){
+		foreach (var item in cubes) {
+			item.SetParent(field.transform,false);
+		}
+
+	}
+}
